f6c24d614335e57abb73f078997d11c74d179c71
Froxtor42 <152212214+Froxtor42@users.noreply.github.com>
2023-12-03 20:17:03 +0100

    Update DirectorAcademico.h

--- DirectorAcademico.h
+++ DirectorAcademico.h
@@ -13,7 +13,8 @@ class DirectorAcademico : public Usuario{
         std::string departamento_;
 
     public:
-        DirectorAcademico(std::string nombre, std::string apellidos, std::string carrera, char[9] dni, std::string correo, std::string departamento) {nombre_=nombre; apellidos_=apellidos; carrera_=carrera; dni_=dni; correo_=correo; departamento_=departamento;}
+        DirectorAcademico(std::string IP_, std::string nombre, std::string apellidos, std::string carrera, char[9] dni, std::string correo, std::string departamento) :  
+        {nombre_=nombre; apellidos_=apellidos; carrera_=carrera; dni_=dni; correo_=correo; departamento_=departamento;}
         std::string GetNombre(return nombre_;)
         std::string GetApellidos(return Apellidos_;)
         std::string GetCarrera(return Carrera_;)
